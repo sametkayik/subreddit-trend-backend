2a15b39149f2e7adc6df8c4faec6cb4fcfdeae72
samet <sametkayik@gmail.com>
2023-03-06 00:46:56 +0300

    Add cors package

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { getSubreddit } from "./controllers/subredditController.js";
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.get("/r/:subreddit", getSubreddit);
 
